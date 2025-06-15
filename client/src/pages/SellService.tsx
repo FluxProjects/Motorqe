d4ae3d7c4f49d0c77e226c54142d705f8d1a777c
Ahsan Shakeel <ahsanshakeel@live.co.uk>
2025-06-15 20:28:50 +0500

    sell service

--- client/src/pages/SellService.tsx
+++ client/src/pages/SellService.tsx
@@ -25,7 +25,7 @@ export default function SellService() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-8">
           <h1 className="text-3xl font-bold text-neutral-900 mb-2">
-            {t("common.sellCar")}
+            {t("common.sellService")}
           </h1>
           <div className="w-40 h-1 bg-orange-500 mx-auto rounded-full" />
         </div>
